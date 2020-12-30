120598d386b80780d62c5f6c0a261b52e0b42c29
Gisele Xiao <yunshu0507@gmail.com>
2020-12-29 19:20:21 -0500

    update RecyclerView

--- app/src/main/java/com/example/mymemo/UI/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/mymemo/UI/RecyclerViewAdapter.java
@@ -1,4 +1,99 @@
 package com.example.mymemo.UI;
 
-public class RecyclerViewAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.mymemo.Model.Grocery;
+import com.example.mymemo.R;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+    private Context context;
+    private List<Grocery> groceryItems;
+
+    public RecyclerViewAdapter(Context context, List<Grocery> groceryItems){
+        this.context = context;
+        this.groceryItems = groceryItems;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_row, parent, false);
+        return new ViewHolder(view, context);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerViewAdapter.ViewHolder holder, int position) {
+
+        Grocery grocery = groceryItems.get(position);
+        holder.groceryItemName.setText(grocery.getName());
+        holder.quantity.setText(grocery.getQuantity());
+        holder.dateAdded.setText(grocery.getDataItemAdded());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return groceryItems.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        public TextView groceryItemName;
+        public TextView quantity;
+        public TextView dateAdded;
+        public Button editButton;
+        public Button deleteButton;
+        public int id;
+
+        public ViewHolder(@NonNull View itemView, Context ctx) {
+            super(itemView);
+
+            context = ctx;
+            groceryItemName = (TextView) itemView.findViewById(R.id.nameRollerView);
+            quantity = (TextView) itemView.findViewById(R.id.quantityRollerView);
+            dateAdded = (TextView) itemView.findViewById(R.id.dateAdded);
+            editButton = (Button) itemView.findViewById(R.id.editButton);
+            deleteButton = (Button) itemView.findViewById(R.id.deleteButton);
+
+            editButton.setOnClickListener(this);
+            deleteButton.setOnClickListener(this);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                  // when click single item, will go to another screen
+                }
+            });
+        }
+
+        @Override
+        public void onClick(View view) {
+
+            switch (view.getId()){
+                case R.id.editButton:
+
+                    break;
+
+                case R.id.deleteButton:
+
+                    break;
+
+            }
+
+
+        }
+    }
 }
